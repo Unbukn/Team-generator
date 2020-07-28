343519cebe45d13173de70e523e6765a3bc5fcaf
James <james.hudgins.work@gmail.com>
2020-07-27 21:55:30 -0400

    added validator dependancy and created prompts for manager

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Dependencies
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const validator = require('validator');
 const path = require("path");
 const fs = require("fs");
 
@@ -9,43 +11,116 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { isNumber } = require("util");
+const { registerPrompt } = require("inquirer");
 
+var allTeamMembers = [];
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
+inquirer.prompt([
+    {
+        // prompt for the manager name
+        type: "input",
+        name: "managerName",
+        message: "What's the name of the project manager(PM)",
+        validate: userEntry => {
+            // if user response is not nothing
+            if(userEntry !== ""){
+                return true;
+            }
+            return "You have to provide a name for the manager?";
+        },
+        default: ""
+    },
+    {
+        // ask for manager's Id
+        type: "input",
+        name: "managerId",
+        message: "What's the manager's Id number?",
+        validate: userEntry => {
+            // if user response is not nothing
+            if(validator.isNumeric(userEntry)){
+                return true;
+            }
+            return "That's not a valid number!";
+        },
+        default: ""
+    },
+    {
+        // ask for manager's email
+        type: "input",
+        name: "managerEmail",
+        message: "What's the manager's email address?",
+        validate: userEntry => {
+            // if user response is not nothing
+            if(validator.isEmail(userEntry)){
+                return true;
+            }
+            return "That's not a valid email address!";
+        },
+        default: ""
+    },
+    {
+        // ask for manager's office number
+        type: "input",
+        name: "managerOffice",
+        message: "What's the manager's office number?",
+        validate: userEntry => {
+            // if user response is not nothing
+            if(validator.isNumeric(userEntry)){
+                return true;
+            }
+            return "That's not a valid number!";
+        },
+        default: ""
+    },
   ])
   .then(answers => {
-    // Use user feedback for... whatever!!
+    // create new class for the manager with user input
+    const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
+    // push the new manager class to allTeamMembers object array
+    allTeamMembers.push(manager);
+    // create the rest of the team 
+    makeTeam() // prompt the user for the rest of the team members
   })
   .catch(error => {
     if(error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
+      return console.log(error.message)
     } else {
       // Something else when wrong
+        return "unknown error occurred"
     }
   });
+// ************** prompt user for the rest of the team members ****************** 
 
+function makeTeam() {
+    inquirer.prompt([
+    {
+        type: "list",
+        name: "memberType",
+        message: "Select a team member to add to the project?",
+        choices: [
+        "Engineer",
+        "Intern",
+        "No additional members need to be added."
+        ],
+        default: ""
+    }
+      ]).then(answers => {
+        switch(answers.memberType) {
+        case "Engineer":
+            // make a new engineer
+          newEng();
+          break;
+        case "Intern":
+            // make new intern profile
+          newIntern();
+          break;
+        default:
+            // generate the team
+          generateTeam();
+        }
+      });
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+};
+  
